a889b446605078aec0f0480dbf00cfac13b50973
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-06-10 10:34:45 +0200

    feat: load .env file

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ from app.core.config import settings
 from app.routers.chat import router as chat_router
 from app.routers.hint import router as hint_router
 from app.routers.text import router as text_router
+from dotenv import load_dotenv
+
 
 
 def get_application():
@@ -22,5 +24,5 @@ def get_application():
     _app.include_router(text_router)
     return _app
 
-
+load_dotenv()
 app = get_application()

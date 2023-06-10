664e0df97f1cec96e679ef952c70f21127d1b034
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-06-10 09:56:32 +0200

    build: basic fastapi project struct

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.core.config import settings
+from app.routers.chat import router as chat_router
+from app.routers.hint import router as hint_router
+from app.routers.text import router as text_router
+
+
+def get_application():
+    _app = FastAPI(title=settings.PROJECT_NAME)
+
+    _app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    _app.include_router(hint_router)
+    _app.include_router(chat_router)
+    _app.include_router(text_router)
+    return _app
+
+
+app = get_application()

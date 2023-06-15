0f00ddbb31239ed5ad5d4a63e474fe1afd35ddf4
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-06-15 09:10:14 +0200

    fix: fix generate public url to audio

--- app/utils/tools/generate_link.py
+++ app/utils/tools/generate_link.py
@@ -5,4 +5,19 @@ import datetime
 
 
 def generate_download_signed_url_v4(bucket_name, blob_name):
-    return f"https://storage.cloud.google.com/{bucket_name}/{blob_name}?authuser=0"   
+    client = storage.Client()
+
+    # Get the bucket
+    bucket = client.get_bucket(bucket_name)
+
+    # Get the blob (file) from the bucket
+    blob = bucket.blob(blob_name)
+
+    # Generate a signed URL for the blob
+    signed_url = blob.generate_signed_url(
+        version="v4",
+        expiration=3600,
+        method="GET"
+    )
+
+    return signed_url

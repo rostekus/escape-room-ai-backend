68ee7e5e919e7397783c878b8f042f343c899b5f
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-06-15 10:28:05 +0200

    fix: add creds for generating link

--- app/utils/tools/generate_link.py
+++ app/utils/tools/generate_link.py
@@ -3,6 +3,20 @@ from google.cloud import storage
 import datetime
 
 
+from google.auth.transport import requests
+from google.auth import default, compute_engine
+    
+credentials, _ = default()
+
+# then within your abstraction
+auth_request = requests.Request()
+credentials.refresh(auth_request)
+
+signing_credentials = compute_engine.IDTokenCredentials(
+    auth_request,
+    "",
+    service_account_email=credentials.service_account_email
+)
 
 def generate_download_signed_url_v4(bucket_name, blob_name):
     client = storage.Client()
@@ -17,6 +31,7 @@ def generate_download_signed_url_v4(bucket_name, blob_name):
     signed_url = blob.generate_signed_url(
         version="v4",
         expiration=3600,
+        credentials=signing_credentials,
         method="GET"
     )
 
